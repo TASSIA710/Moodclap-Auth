3003a6c03a4d26ac030b409a850892354779ed62
Tassilo <tassia710@gmail.com>
2020-05-18 13:34:30 +0200

    Changed default brand URL

--- Configuration.php
+++ Configuration.php
@@ -3,7 +3,7 @@
 const AUTH_CONFIG = [
 
 	'BRAND_NAME' => 'Tassilo',
-	'BRAND_URL' => 'http://localhost',
+	'BRAND_URL' => 'http://example.com',
 	'ROOT' => '/',
 	'WINDOW_TITLE' => 'Auth | Tassilo',
 
