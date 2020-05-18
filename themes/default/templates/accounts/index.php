8725758feb2946d08f3315fbff444faa02b44a39
Tassilo <tassia710@gmail.com>
2020-05-18 17:31:21 +0200

    Added group name to accounts index

--- themes/default/templates/accounts/index.php
+++ themes/default/templates/accounts/index.php
@@ -3,7 +3,7 @@ include(__DIR__ . '/../misc/header.php');
 ?>
 
 <span class="display-4"><?= Utility::escapeXSS($ACCOUNT->getUsername()); ?></span><br>
-<span class="text-muted">User #<?= Utility::escapeXSS($ACCOUNT->getID()); ?></span>
+<span class="text-muted">User #<?= Utility::escapeXSS($ACCOUNT->getID()) . ' | ' . Utility::escapeXSS($ACCOUNT->getGroup()->getName()); ?></span>
 
 <?php
 include(__DIR__ . '/../misc/footer.php');
