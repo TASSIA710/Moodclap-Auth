4cdfa366f3009949deb3a7e639ac767ad1976fda
Tassilo <tassia710@gmail.com>
2020-05-18 16:26:50 +0200

    Populated accounts page a little bit

--- themes/default/templates/accounts/index.php
+++ themes/default/templates/accounts/index.php
@@ -1,4 +1,9 @@
 <?php
-
 include(__DIR__ . '/../misc/header.php');
+?>
+
+<span class="display-4"><?= Utility::escapeXSS($ACCOUNT->getUsername()); ?></span><br>
+<span class="text-muted">User #<?= Utility::escapeXSS($ACCOUNT->getID()); ?></span>
+
+<?php
 include(__DIR__ . '/../misc/footer.php');
