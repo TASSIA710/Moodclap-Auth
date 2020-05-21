c04238b94b908acbfd067a6bb30f62472d566e5c
Tassilo <tassia710@gmail.com>
2020-05-21 20:48:32 +0200

    Added permission constants

--- core/Constants.php
+++ core/Constants.php
@@ -1,5 +1,6 @@
 <?php
 
+/* == -- Version Constants -- == */
 const MC_AC_VER_MAJOR = 1;
 const MC_AC_VER_MINOR = 0;
 const MC_AC_VER_PATCH = 0;
@@ -8,3 +9,15 @@ const MC_AC_VER_BUILD = 1;
 const MC_AC_VER_MM = MC_AC_VER_MAJOR . '.' . MC_AC_VER_MINOR;
 const MC_AC_VER_MMP = MC_AC_VER_MM . '.' . MC_AC_VER_PATCH;
 const MC_AC_VER_MMPB = MC_AC_VER_MMP . ', build ' . MC_AC_VER_BUILD;
+/* == -- Version Constants -- == */
+
+
+
+
+
+/* == -- Permission Constants -- == */
+const PERM_AC_GROUPS_VIEW = 'mc.ac.groups.view';
+const PERM_AC_GROUPS_EDIT = 'mc.ac.groups.edit';
+const PERM_AC_GROUPS_CREATE = 'mc.ac.groups.create';
+const PERM_AC_GROUPS_DELETE = 'mc.ac.groups.delete';
+/* == -- Permission Constants -- == */
