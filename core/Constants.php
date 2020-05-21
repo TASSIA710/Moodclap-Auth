cfb5a37573e23cb173cac5dd7b7ae804a85b9f07
Tassilo <tassia710@gmail.com>
2020-05-21 18:08:31 +0200

    Added version constants

---
+++ core/Constants.php
@@ -0,0 +1,10 @@
+<?php
+
+const MC_AC_VER_MAJOR = 1;
+const MC_AC_VER_MINOR = 0;
+const MC_AC_VER_PATCH = 0;
+const MC_AC_VER_BUILD = 1;
+
+const MC_AC_VER_MM = MC_AC_VER_MAJOR . '.' . MC_AC_VER_MINOR;
+const MC_AC_VER_MMP = MC_AC_VER_MM . '.' . MC_AC_VER_PATCH;
+const MC_AC_VER_MMPB = MC_AC_VER_MMP . ', build ' . MC_AC_VER_BUILD;
