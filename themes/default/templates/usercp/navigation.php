131891f353c9914753a6662e20bced53ab58005f
Tassilo <tassia710@gmail.com>
2020-05-23 16:15:28 +0200

    Added UserCP navigation

---
+++ themes/default/templates/usercp/navigation.php
@@ -0,0 +1,25 @@
+
+<h6 class="text-muted"><i class="fas fa-user-cog mr-2"></i>USER CONTROL PANEL</h6>
+
+
+<hr>
+
+
+<a href="<?= AUTH_CONFIG['ROOT']; ?>usercp/"><i class="fas fa-home mr-2"></i>Overview</a>
+<br>
+
+<a href="<?= AUTH_CONFIG['ROOT']; ?>usercp/sessions/"><i class="fas fa-desktop mr-2"></i>Sessions</a>
+<br>
+
+<a href="<?= AUTH_CONFIG['ROOT']; ?>usercp/password/"><i class="fas fa-key mr-2"></i>Change Password</a>
+<br>
+
+
+<hr>
+
+
+<a href="<?= AUTH_CONFIG['ROOT']; ?>logout/" class="text-danger"><i class="fas fa-sign-out-alt mr-2"></i>Logout</a>
+<br>
+
+<a href="<?= AUTH_CONFIG['ROOT']; ?>usercp/disable/" class="text-danger"><i class="fas fa-ban mr-2"></i>Disable Account</a>
+<br>
